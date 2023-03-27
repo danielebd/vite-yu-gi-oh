2030a47f16a1830d9a6faa2c13f11cf374b7ca52
danielebd <119417050+danielebd@users.noreply.github.com>
2023-03-27 16:26:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue Template
+
+Template di partenza per progetti Vue con option API
+
+Per installare il progetto
+
+- npm install
+- npm run dev
+
---
+++ src/components/HelloApp.vue
@@ -0,0 +1,16 @@
+
+<script>
+export default {
+    name: 'HelloApp'
+}
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
