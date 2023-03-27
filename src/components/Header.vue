b3a64bf12e45abcb88ceb2e053a0743261dda7fd
danielebd <danielebalebalestro@gmail.com>
2023-03-27 17:32:09 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,11 +6,21 @@ export default {
 </script>
 
 <template>
-    <div>
-
-    </div>
+    <header class="d-flex align-items-center p-2">
+        <div class="logo me-2">
+            <img src="http://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es" alt="">
+        </div>
+        <h2>Yu-Gi-Oh Api</h2>
+    </header>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    header {
+        .logo {
+            width: 75px;
+            img {
+                width: 100%;
+            }
+        }
+    }
 </style>
