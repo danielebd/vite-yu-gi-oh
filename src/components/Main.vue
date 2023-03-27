72e5c7f68314a10cb6b5ea89c312323f505c22a2
danielebd <danielebalebalestro@gmail.com>
2023-03-27 17:40:39 +0200

    drop down

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,23 @@
 
 <script>
- export default {
+export default {
     name: 'Main'
- }
+}
 </script>
 
 <template>
-    <div>
-
-    </div>
+    <main>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                archetype
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </main>
 </template>
 <style lang="scss" scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import './assets/scss/main.scss';
-import "@fontsource/montserrat"; // Defaults to weight 400.
+import "@fontsource/montserrat";
+import * as bootstrap from 'bootstrap'
+ // Defaults to weight 400.
 import App from './App.vue';
 
 createApp(App).mount('#app')
