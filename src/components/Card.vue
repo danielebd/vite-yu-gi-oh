07a4947f827da0e568c6e23324748ef5fc651bc8
danielebd <danielebalebalestro@gmail.com>
2023-03-28 11:12:21 +0200

    upload 1.1

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -18,7 +18,7 @@ export default {
         <div class="name text-center text-light fw-bold py-2">
             {{ name }}
         </div>
-        <div class="archetype text-center pb-2">
+        <div class="archetype text-center pb-2" >
             {{ archetype }}
         </div>
     </div>
@@ -29,9 +29,14 @@ export default {
         width: calc(100% / 5 - 15px);
         background-color: #d48f38;
         margin: 0 7.5px;
+        border: 0;
+        .name {
+            height: 100%;
+        }
         img {
             width: 100%;
             background-image: url('https://www.grouphealth.ca/wp-content/uploads/2018/05/placeholder-image.png');
+            
         }
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue';
 import './assets/scss/main.scss';
 import "@fontsource/montserrat";
-import * as bootstrap from 'bootstrap'
  // Defaults to weight 400.
+import * as bootstrap from 'bootstrap';
+
 import App from './App.vue';
 
 createApp(App).mount('#app')
